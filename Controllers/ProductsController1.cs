f415f7cd3f934914630f2b5d1e8a51e4d3def850
MKA <MKA@A26PC3>
2024-09-14 13:21:28 +0300

    Add project files.

---
+++ Controllers/ProductsController1.cs
@@ -0,0 +1,27 @@
+﻿using Cosmetics.Helpers;
+using Cosmetics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cosmetics.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //product list
+        List<Product> products;
+        public ProductsController()
+        {
+            products = new List<Product>(Seeder.GetProducts());
+        }
+        public IActionResult Index()
+        {
+            //TODO : get data from DB
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null) { return NotFound(); }
+            return View(product);
+        }
+    }
+}
